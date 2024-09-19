e389966b66b347c6e80eff8462ffe09596b00edc
ohyoxo <126952400+ohyoxo@users.noreply.github.com>
2024-09-19 18:47:21 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,106 @@
-const { spawn } = require('child_process');
-const fs = require('fs');
+const axios = require('axios');
+const http = require('http');
+const cron = require('node-cron');
+const port = process.env.PORT || 7860;     
+const moment = require('moment-timezone');
 
-const startScriptPath = './start.sh';
-const interpreterPath = '/usr/bin/env';
-const interpreterArgs = ['bash', startScriptPath];
+// 添加24小时访问的URL数组
+const urls = [
+  'https://www.baidu.com',             // 此处可备注名称，例如：glitch
+  'https://www.yahoo.com',             // 此处可备注名称，例如：glitch
+  'https://www.baidu.com',             // 此处可备注名称，例如：glitch
+  'https://www.yahoo.com',             // 此处可备注名称，例如：glitch
+  'https://www.baidu.com',             // 此处可备注名称，例如：glitch
+  'https://www.yahoo.com',             // 此处可备注名称，例如：glitch
+  'https://www.baidu.com',             // 此处可备注名称，例如：glitch
+  'https://www.yahoo.com',             // 此处可备注名称，例如：glitch
+  // 添加更多24小时不间断访问的URL
+];
 
-try {
-  fs.chmodSync(startScriptPath, 0o755);
-  console.log(`赋权成功: ${startScriptPath}`);
-} catch (error) {
-  console.error(`赋权失败: ${error}`);
+// 添加在01:00至05:00暂停访问，其他时间正常访问的URL数组
+function visitWebsites() {
+  const websites = [
+    'https://www.google.com',        // 此处可备注名称，例如：Back4app
+    'https://www.google.com',        // 此处可备注名称，例如：Back4app
+    'https://www.google.com',        // 此处可备注名称，例如：Back4app
+    'https://www.google.com'         // 此处可备注名称，例如：Back4app，最后一个url后面没有逗号
+    //添加更多的指定时间访问的URL
+  ];
+
+ // 遍历网页数组并发送请求
+  websites.forEach(async (url) => {
+    try {
+      const response = await axios.get(url);
+      console.log(`${moment().tz('Asia/Hong_Kong').format('YYYY-MM-DD HH:mm:ss')} Visited web successfully: ${url} - Status code: ${response.status}\n`);
+    } catch (error) {
+      console.error(`Error visiting ${url}: ${error.message}\n`);
+    }
+  });
 }
 
-const startScript = spawn(interpreterPath, interpreterArgs);
+// 检查并设置定时器
+function checkAndSetTimer() {
+  const currentMoment = moment().tz('Asia/Hong_Kong');
+  const formattedTime = currentMoment.format('YYYY-MM-DD HH:mm:ss');
+  if (currentMoment.hours() >= 1 && currentMoment.hours() < 5) {
+    console.log(`Stop visit from 1:00 to 5:00 --- ${moment().tz('Asia/Hong_Kong').format('YYYY-MM-DD HH:mm:ss')}`);
+    clearInterval(visitIntervalId); // 清除定时器
+    const nextVisitTime = moment().tz('Asia/Hong_Kong').add(0, 'day').hours(5).minutes(0).seconds(0);
+    const nextVisitInterval = nextVisitTime.diff(currentMoment);
+    setTimeout(() => {
+      startVisits();
+    }, nextVisitInterval);
+  } else {
+    startVisits();
+  }
+}
 
-startScript.stdout.on('data', (data) => {
-  console.log(`输出：${data}`);
+let visitIntervalId; 
+function startVisits() {
+  clearInterval(visitIntervalId);
+// visitWebsites();
+  visitIntervalId = setInterval(() => {
+    visitWebsites();
+  }, 2 * 60 * 1000);   // 每2分钟执行一次访问
+}
+
+function runScript() {
+  const runScriptIntervalId = setInterval(() => {
+    //console.log('Running script');
+    checkAndSetTimer();
+  }, 2 * 60 * 1000); // 每2分钟检查一次
+}
+checkAndSetTimer();
+runScript();
+
+// 24小时不间断访问
+async function scrapeAndLog(url) {
+  try {
+    const response = await axios.get(url);
+    console.log(`${moment().tz('Asia/Hong_Kong').format('YYYY-MM-DD HH:mm:ss')} Web visited Successfully: ${url} - Status code: ${response.status}\n`);
+  } catch (error) {
+    console.error(`${moment().tz('Asia/Hong_Kong').format('YYYY-MM-DD HH:mm:ss')}: Web visited Error: ${url}: ${error.message}\n`);
+  }
+}
+// 每2分钟访问一次
+cron.schedule('*/2 * * * *', () => {
+  console.log('Running webpage access...');
+  urls.forEach((url) => {
+    scrapeAndLog(url);
+  });
 });
 
-startScript.stderr.on('data', (data) => {
-  console.error(`${data}`);
+// 创建HTTP服务
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello, World!\n');
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('Not Found\n');
+  }
 });
 
-startScript.on('error', (error) => {
-  console.error(`启动脚本错误: ${error}`);
-  process.exit(1); 
-});
-
-startScript.on('close', (code) => {
-  console.log(`子进程退出，退出码 ${code}`);
+server.listen(port, () => {
+  console.log(`Server is running on port:${port}`);
 });
